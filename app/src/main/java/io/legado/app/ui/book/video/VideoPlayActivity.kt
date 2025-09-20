257960690dd9f646313f7cd9593480ec1d26f794
薯条 <gv92207@gmail.com>
2025-09-21 02:12:27 +0800

    添加视频播放界面

---
+++ app/src/main/java/io/legado/app/ui/book/video/VideoPlayActivity.kt
@@ -0,0 +1,157 @@
+package io.legado.app.ui.book.video
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackParameters
+import androidx.media3.exoplayer.ExoPlayer
+import io.legado.app.R
+import io.legado.app.base.VMBaseActivity
+import io.legado.app.constant.Theme
+import io.legado.app.databinding.ActivityAudioPlayBinding
+import io.legado.app.databinding.ActivityAudioPlayBinding.inflate
+import io.legado.app.databinding.ActivityVideoPlayBinding
+import io.legado.app.ui.book.audio.AudioPlayViewModel
+import io.legado.app.ui.book.read.page.provider.ZhLayout
+import io.legado.app.utils.viewbindingdelegate.viewBinding
+import java.util.Locale
+import kotlin.getValue
+import kotlin.math.abs
+
+class VideoPlayActivity(
+) : VMBaseActivity<ActivityVideoPlayBinding, VideoViewModel>(toolBarTheme = Theme.Dark) {
+    private var player: ExoPlayer? = null
+    override val binding by viewBinding(ActivityVideoPlayBinding::inflate)
+    override val viewModel by viewModels<VideoViewModel>()
+
+    private var isFullscreen = false
+private lateinit var gestureDetector: GestureDetector
+private var currentSpeed = 1f
+    private var originalSpeed = 1f
+//    private var position: Long = 0
+    private val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+    private var endX = 0f
+    private var startX = 0f
+    private var position: Long = 0L
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+    player = ExoPlayer.Builder(this).build().apply {
+        binding.ivPlayer.player = this
+        val mediaItem = MediaItem.fromUri("https://media.w3.org/2010/05/sintel/trailer.mp4")
+        setMediaItem(mediaItem)
+        prepare()
+        playWhenReady = true
+    }
+        val duration by lazy { player!!.duration}
+        gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onLongPress(e: MotionEvent) {
+                val player = player ?: return
+                originalSpeed = player.playbackParameters.speed
+                currentSpeed = originalSpeed * 2f
+                player.playbackParameters = PlaybackParameters(currentSpeed, player.playbackParameters.pitch)
+                binding.tvVideoSpeed.text = "${currentSpeed}x"
+                binding.tvVideoSpeed.visibility = View.VISIBLE
+            }
+
+            override fun onScroll(e1: MotionEvent?, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+                if (abs(distanceX) > abs(distanceY)) {
+
+                val player = player ?: return false
+                val currentPosition = player.currentPosition
+                endX += distanceX
+
+                position = (currentPosition - (endX / screenWidth) * 120000).toLong().coerceIn(0, duration)
+                binding.tvVideoSpeed.text = position.toString()
+                //formatDuration(position)
+                binding.tvVideoSpeed.visibility = View.VISIBLE
+                return true
+            }
+            return false
+       }
+})
+
+// 设置全屏按钮点击事件
+//binding.fullscreenButton.setOnClickListener {
+//    toggleFullscreen()
+//}
+
+binding.ivPlayer.setOnTouchListener { v, event ->
+    val handled = gestureDetector.onTouchEvent(event)
+
+    if (event.action == MotionEvent.ACTION_DOWN) {
+        v.performClick()
+    }
+
+    if (event.action == MotionEvent.ACTION_UP) {
+        if(!handled) {
+
+            currentSpeed = originalSpeed
+            player?.playbackParameters = player?.playbackParameters?.let {
+                PlaybackParameters(currentSpeed, it.pitch)
+            } ?: PlaybackParameters(currentSpeed)
+        }else{
+            player?.seekTo(position)
+//            player?.play()
+        }
+        binding.tvVideoSpeed.visibility = View.GONE
+        endX = 0f
+    }
+
+
+    true
+}
+
+
+}
+
+    private fun toggleFullscreen() {
+    isFullscreen = !isFullscreen
+    if (isFullscreen) {
+        // 进入全屏
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+        binding.ivPlayer.layoutParams = binding.ivPlayer.layoutParams.apply {
+            width = WindowManager.LayoutParams.MATCH_PARENT
+            height = WindowManager.LayoutParams.MATCH_PARENT
+        }
+//        binding.fullscreenButton.setImageResource(R.drawable.ic_exit_fullscreen)
+    } else {
+        // 退出全屏
+        window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        binding.ivPlayer.layoutParams = binding.ivPlayer.layoutParams.apply {
+            width = WindowManager.LayoutParams.MATCH_PARENT
+            height = WindowManager.LayoutParams.WRAP_CONTENT
+        }
+//        binding.fullscreenButton.setImageResource(R.drawable.ic_enter_fullscreen)
+    }
+}
+
+private fun formatDuration(duration: Long): String {
+    val hours = duration / 3600000
+    val minutes = (duration % 3600000) / 60000
+    val seconds = (duration % 60000) / 1000
+    return if (hours > 0) {
+        String.format(Locale.ROOT,"HH:mm:ss", hours, minutes, seconds)
+    } else {
+        String.format(Locale.ROOT,"mm:ss", minutes, seconds)
+    }
+}
+
+
+override fun onDestroy() {
+    super.onDestroy()
+    player?.release()
+}
+}
---
+++ app/src/main/java/io/legado/app/ui/book/video/VideoViewModel.kt
@@ -0,0 +1,8 @@
+package io.legado.app.ui.book.video
+
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+class VideoViewModel(application: Application):BaseViewModel(application) {
+
+}
