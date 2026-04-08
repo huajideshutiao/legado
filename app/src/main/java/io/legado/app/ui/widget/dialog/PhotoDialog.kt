3c7b58871355b5d4bf3ecaa664083425c25ef8f1
huajideshutiao <huajideshutiao@users.noreply.github.com>
2026-04-08 16:18:22 +0000

    feat: 长按保存图片功能
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- app/src/main/java/io/legado/app/ui/widget/dialog/PhotoDialog.kt
+++ app/src/main/java/io/legado/app/ui/widget/dialog/PhotoDialog.kt
@@ -4,21 +4,28 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy
 import com.bumptech.glide.request.RequestOptions
 import io.legado.app.R
 import io.legado.app.base.BaseDialogFragment
+import io.legado.app.constant.AppConst.imagePathKey
 import io.legado.app.databinding.DialogPhotoViewBinding
 import io.legado.app.help.book.BookHelp
 import io.legado.app.help.glide.ImageLoader
 import io.legado.app.help.glide.OkHttpModelLoader
+import io.legado.app.lib.dialogs.SelectItem
 import io.legado.app.model.BookCover
 import io.legado.app.model.ImageProvider
 import io.legado.app.model.ReadBook
+import io.legado.app.ui.file.HandleFileContract
+import io.legado.app.utils.ACache
 import io.legado.app.utils.setLayout
+import io.legado.app.utils.toast
 import io.legado.app.utils.viewbindingdelegate.viewBinding
+import kotlinx.coroutines.launch
 
 /**
  * 显示图片
@@ -33,6 +40,13 @@ class PhotoDialog() : BaseDialogFragment(R.layout.dialog_photo_view) {
     }
 
     private val binding by viewBinding(DialogPhotoViewBinding::bind)
+    private var imageSrc: String? = null
+    private val saveImage = registerForActivityResult(HandleFileContract()) {
+        it.uri?.let { uri ->
+            ACache.get().put(imagePathKey, uri.toString())
+            saveImage(imageSrc, uri)
+        }
+    }
 
     override fun onStart() {
         super.onStart()
@@ -43,8 +57,10 @@ class PhotoDialog() : BaseDialogFragment(R.layout.dialog_photo_view) {
     override fun onFragmentCreated(view: View, savedInstanceState: Bundle?) {
         val arguments = arguments ?: return
         val src = arguments.getString("src") ?: return
+        imageSrc = src
         ImageProvider.get(src)?.let {
             binding.photoView.setImageBitmap(it)
+            setLongClick()
             return
         }
         val file = ReadBook.book?.let { book ->
@@ -66,6 +82,55 @@ class PhotoDialog() : BaseDialogFragment(R.layout.dialog_photo_view) {
                 .dontTransform()
                 .downsample(DownsampleStrategy.NONE)
                 .into(binding.photoView)
+        }
+        setLongClick()
+    }
+
+    private fun setLongClick() {
+        binding.photoView.setOnLongClickListener {
+            saveImage(imageSrc)
+            true
+        }
+    }
+
+    private fun saveImage(url: String?) {
+        url?.let {
+            this.imageSrc = it
+            val path = ACache.get().getAsString(imagePathKey)
+            if (path.isNullOrEmpty()) {
+                selectSaveFolder(it)
+            } else {
+                saveImage(it, path.toUri())
+            }
+        }
+    }
+
+    private fun selectSaveFolder(url: String) {
+        this.imageSrc = url
+        val default = arrayListOf<SelectItem<Int>>()
+        val path = ACache.get().getAsString(imagePathKey)
+        if (!path.isNullOrEmpty()) {
+            default.add(SelectItem(path, -1))
+        }
+        saveImage.launch {
+            otherActions = default
+        }
+    }
+
+    private fun saveImage(url: String?, uri: android.net.Uri) {
+        url?.let {
+            lifecycleScope.launch {
+                kotlin.runCatching {
+                    val bitmap = ImageProvider.get(it) ?: return@launch
+                    val outputStream = requireContext().contentResolver.openOutputStream(uri)
+                    outputStream?.use {out ->
+                        bitmap.compress(android.graphics.Bitmap.CompressFormat.PNG, 100, out)
+                    }
+                    toast(R.string.save_success)
+                }.onFailure {
+                    toast(R.string.save_fail)
+                }
+            }
         }
     }
 
